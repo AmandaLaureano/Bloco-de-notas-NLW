3d01e415f322c1abe150dc91e7f4a49a683e34f1
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2024-02-06 14:28:10 -0300

    Tela Inicial e componentes card

--- README.md
+++
@@ -1,2 +0,0 @@
-# Bloco-de-notas-NLW
- Uma aplicação de bloco de notas desenvolvida seguindo o evento NWL da Rocketseat.
---
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -0,0 +1,11 @@
+export function NoteCard(){
+    return(
+        <button className='rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 outline-none hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+            <span className='text-sm font-medium text-slate-300'>Há 2 dias</span>
+            <p className='text-sm leading-6 text-slate-400'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde beatae nihil soluta fugit reiciendis cumque amet asperiores qui illum, dolore voluptate, necessitatibus aliquam. Praesentium facere est, amet excepturi tenetur quaerat.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde beatae nihil soluta fugit reiciendis cumque amet asperiores qui illum, dolore voluptate, necessitatibus aliquam. Praesentium facere est, amet excepturi tenetur quaerat.
+            </p>
+            <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
+        </button>
+    )
+}
