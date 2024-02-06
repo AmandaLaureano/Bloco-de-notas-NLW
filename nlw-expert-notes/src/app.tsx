00e9a6890b2bab7014323b6439f09162f15bbc2a
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2024-02-06 17:57:56 -0300

    Criação de funções formulário

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -2,6 +2,11 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+const note ={
+  date: new Date(),
+  content: 'OIIII'
+}
+
 export function App() {
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -16,11 +21,7 @@ export function App() {
       <div className='h-px bg-slate-700'/>
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard/>
-        <NoteCard/>
-        <NoteCard/>
-        <NoteCard/>
-        <NoteCard/>
-        <NoteCard/>
+        <NoteCard note={note}/>
       </div>
     </div>
   )
--- nlw-expert-notes/src/components/new-note-card.tsx
+++ nlw-expert-notes/src/components/new-note-card.tsx
@@ -1,10 +1,71 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'lucide-react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { toast } from 'sonner'
+
 export function NewNoteCard(){
+  const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true)
+  const [content, setContent] = useState('')
+
+    function handleStartEditor(){
+      setShouldShowOnBoarding(false)
+    }
+
+    function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>){
+      setContent(event.target.value)
+      
+      if (event.target.value === ''){
+        setShouldShowOnBoarding(true)
+      }
+    }
+
+    function handleSaveNote(event: FormEvent){
+      event.preventDefault()
+      console.log(content)
+
+      toast.success('Nota criada com sucesso!')
+    }
+
     return(
-        <div className='rounded-md bg-slate-700 p-5 space-y-3 overflow-hidden'>
+
+      <Dialog.Root>
+        <Dialog.Trigger className='rounded-md flex flex-col text-left bg-slate-700 p-5 gap-3 overflow-hidden hover:ring-2 outline-none hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
           <span className='text-sm font-medium text-slate-200'>Adicionar Nota</span>
           <p className='text-sm leading-6 text-slate-400'>
             Grave uma nota em áudio que será convertida para texto automaticamente.
           </p>
-        </div>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className='inset-0 fixed bg-black/50'/>
+            <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+                <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
+                    <X className='size-5'/>
+                </Dialog.Close>
+                <form onSubmit={handleSaveNote} className='flex-1 flex flex-col'>
+                  <div className='flex flex-1 flex-col gap-3 p-5'>
+                      <span className='text-sm font-medium text-slate-300'>
+                        Adicionar nota
+                      </span>
+                      {shouldShowOnBoarding ? (
+                        <p className='text-sm leading-6 text-slate-400'>
+                          Comece <button className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className='font-medium text-lime-400 hover:underline'>utilize apenas texto.</button>
+                        </p>
+                      ): (
+                        <textarea 
+                        className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
+                        autoFocus
+                        onChange={handleContentChanged}
+                        />
+                      )}
+                  </div>
+                </form>
+                <button 
+                className='w-full bg-lime-400 text-lime-950 py-4 text-center text-sm outline-none font-medium hover:bg-lime-500'
+                type='submit'>
+                  Salvar nota
+                </button>
+            </Dialog.Content>
+          </Dialog.Portal>
+      </Dialog.Root>
     )
 }
