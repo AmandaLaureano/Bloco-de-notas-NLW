2897c338ceb2e85b954683204904ab85b5cb0324
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2024-02-09 17:30:56 -0300

    Passar função por props

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react'
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
-const note ={
-  date: new Date(),
-  content: 'OIIII'
-}
-
 export function App() {
+  const [notes, setNotes] = useState([
+    {id: 1, date: new Date(), content: 'Hello World'}
+  ])
+
+  function onNoteCreated(content: string){
+    const newNote = {
+      id: Math.random(),
+      date: new Date(),
+      content,
+    }
+    setNotes([newNote, ...notes ])
+  }
+
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt='NLW Expert'/>
@@ -20,8 +29,11 @@ export function App() {
       </form>
       <div className='h-px bg-slate-700'/>
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-        <NewNoteCard/>
-        <NoteCard note={note}/>
+        <NewNoteCard onNoteCreated={onNoteCreated}/>
+
+        {notes.map(note =>{
+          return <NoteCard key={note.id} note={note}/>
+        })}
       </div>
     </div>
   )
--- nlw-expert-notes/src/components/new-note-card.tsx
+++ nlw-expert-notes/src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { toast } from 'sonner'
 
-export function NewNoteCard(){
+interface NewNoteCardProps{
+  onNoteCreated: (content: string) => void
+}
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps){
   const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true)
   const [content, setContent] = useState('')
 
@@ -21,7 +25,7 @@ export function NewNoteCard(){
 
   function handleSaveNote(event: FormEvent){
     event.preventDefault()
-    console.log(content)
+    onNoteCreated(content)
     toast.success('Nota criada com sucesso!')
   }
 
